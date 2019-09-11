d3df86e4f9646b93cc0ee145be46ea5b3035894b
Oliver Lau <ola@ct.de>
2019-09-11 15:59:09 +0200

    Changed copyright notice.

--- pwned-lib/util.hpp
+++ pwned-lib/util.hpp
@@ -1,5 +1,5 @@
 /*
- Copyright © 2019 Oliver Lau <oliver@ersatzworld.net>
+ Copyright © 2019 Oliver Lau <ola@ct.de>, Heise Medien GmbH & Co. KG - Redaktion c't
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
