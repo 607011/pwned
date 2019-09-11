0254911d424607223c35061a070c709f34f4dc17
Oliver Lau <ola@ct.de>
2019-09-11 15:30:15 +0200

    Added command-line options to test-set-extractor.

--- test-set-extractor/test-set-extractor.cpp
+++ test-set-extractor/test-set-extractor.cpp
@@ -21,24 +21,97 @@
 
 #include <sys/types.h>
 
+#include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/random/mersenne_twister.hpp>
 
 #include <passwordhashandcount.hpp>
 #include <passwordinspector.hpp>
 
+namespace po = boost::program_options;
+
+po::options_description desc("Allowed options");
+
+void hello()
+{
+  std::cout << "#pwned lookup benchmark 1.0-RC - Copyright (c) 2019 Oliver Lau" << std::endl
+            << std::endl;
+}
+
+void license()
+{
+  std::cout << "This program comes with ABSOLUTELY NO WARRANTY; for details type" << std::endl
+            << "`test-set-extractor --warranty'." << std::endl
+            << "This is free software, and you are welcome to redistribute it" << std::endl
+            << "under certain conditions; see https://www.gnu.org/licenses/gpl-3.0.en.html" << std::endl
+            << "for details." << std::endl
+            << std::endl;
+}
+
+void warranty()
+{
+  std::cout << "Warranty info:"
+            << std::endl
+            << std::endl
+            << "THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION."
+            << std::endl
+            << std::endl
+            << "IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES."
+            << std::endl
+            << std::endl;
+}
+
+void usage()
+{
+  std::cout << desc << std::endl;
+}
+
 int main(int argc, const char *argv[])
 {
-  if (argc < 3)
+  hello();
+  std::string inputFilename;
+  std::string outputFilename;
+  static constexpr int DefaultN = 20000;
+  int N = DefaultN;
+  bool onlyNonExistent = false;
+  desc.add_options()
+  ("help", "produce help message")
+  ("input,I", po::value<std::string>(&inputFilename), "set user:pass input file")
+  ("output,O", po::value<std::string>(&outputFilename), "set user:pass test set file")
+  ("num,N", po::value<int>(&N)->default_value(DefaultN), "number of data sets to extract")
+  ("non-existent", po::bool_switch(&onlyNonExistent)->default_value(false), "select only non-existing hashes (or else only hashes contained in the input file will be selected)");
+  // ("warranty", po::bool_switch(), "display warranty information")
+  // ("license", po::bool_switch(), "display license information");
+  po::variables_map vm;
+  try
   {
-    std::cerr << "Usage: test-set-extractor <md5_count_input_file> <md5_count_output_file>" << std::endl;
-    return EXIT_FAILURE;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
   }
-  const std::string &inputFilename = argv[1];
-  const std::string &outputFilename = argv[2];
-  const uint64_t N = 20000;
+  catch (po::error &e)
+  {
+    std::cerr << "ERROR: " << e.what() << std::endl
+              << std::endl;
+    usage();
+  }
+  po::notify(vm);
+  if (vm.count("help"))
+  {
+    usage();
+    return EXIT_SUCCESS;
+  }
+  if (vm.count("warranty"))
+  {
+    warranty();
+    return EXIT_SUCCESS;
+  }
+  if (vm.count("license"))
+  {
+    license();
+    return EXIT_SUCCESS;
+  }
+
   const uint64_t size = boost::filesystem::file_size(inputFilename);
-  const uint64_t offset = size / N;
+  const uint64_t offset = size / uint64_t(N);
   std::ifstream in(inputFilename, std::ios::binary);
   if (!in.is_open())
   {
@@ -55,33 +128,41 @@ int main(int argc, const char *argv[])
   std::cout << "Input file:  " << inputFilename << " (" << size << " bytes, offset = " << offset << ")" << std::endl
             << "Output file: " << outputFilename << std::endl;
 
-  std::cout << "Selecting " << N << " present hashes ... " << std::endl;
-  pwned::PHC phc;
-  for (uint64_t pos = 0; pos < size; pos += offset)
+  if (onlyNonExistent)
   {
-    const uint64_t idx = pos - pos % pwned::PHC::size;
-    if (phc.read(in, idx))
+    std::cout << "Selecting " << N << " non-existent hashes ... " << std::endl;
+    boost::random::mt19937_64 gen;
+    gen.seed(31337);
+    pwned::PasswordInspector inspector(inputFilename);
+    for (auto i = 0; i < N; ++i)
     {
-      std::cout << phc.hash << " @ " << idx << std::endl;
-      phc.dump(out);
+      pwned::Hash hash(gen(), gen());
+      pwned::PHC p = inspector.binsearch(hash);
+      if (p.count == 0)
+      {
+        p.hash = hash;
+        p.dump(out);
+        std::cout << hash << " #" << i << std::endl;
+      }
+    }
+  }
+  else
+  {
+    std::cout << "Selecting " << N << " existent hashes ... " << std::endl;
+    pwned::PHC phc;
+    int i = 0;
+    for (uint64_t pos = 0; pos < size && i < N; pos += offset)
+    {
+      const uint64_t idx = pos - pos % pwned::PHC::size;
+      if (phc.read(in, idx))
+      {
+        std::cout << phc.hash << " @ " << idx << std::endl;
+        phc.dump(out);
+        ++i;
+      }
     }
   }
 
-  std::cout << "Selecting " << N << " non-existent hashes ... " << std::endl;
-  boost::random::mt19937_64 gen;
-  gen.seed(31337);
-  pwned::PasswordInspector inspector(inputFilename);
-  for (auto i = 0; i < N; ++i)
-  {
-    pwned::Hash hash(gen(), gen());
-    pwned::PHC p = inspector.binsearch(hash);
-    if (p.count == 0)
-    {
-      p.hash = hash;
-      p.dump(out);
-      std::cout << hash << " #" << i << std::endl;
-    }
-  }
   std::cout << std::endl
             << "Ready." << std::endl;
   return EXIT_SUCCESS;
