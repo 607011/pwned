50c7227dd11c685bdcd0284f93247fd2cabe2623
Oliver Lau <ola@ct.de>
2019-09-12 12:59:56 +0200

    Added a static_cast for clarity.

--- pwned-converter-cli/convertoperation.cpp
+++ pwned-converter-cli/convertoperation.cpp
@@ -57,7 +57,7 @@ ConvertOperation::ConvertOperation(const std::string &srcFilename,
                                                                              maxMem,
                                                                              options)))
 {
-  priority = fs::file_size(srcFilename);
+  priority = static_cast<long long>(fs::file_size(srcFilename));
 }
 
 void ConvertOperation::execute() noexcept(false)
