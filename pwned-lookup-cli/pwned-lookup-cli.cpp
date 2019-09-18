3cdf5dc4b5874366e8cdcbfce8fe61eeb1c66363
Oliver Lau <ola@ct.de>
2019-09-18 10:23:27 +0200

    pwned-lookup-cli can now be fed with index file.

--- pwned-lookup-cli/pwned-lookup-cli.cpp
+++ pwned-lookup-cli/pwned-lookup-cli.cpp
@@ -16,12 +16,15 @@
  */
 
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include <string>
 #include <chrono>
 
+#include <boost/program_options.hpp>
+
 #include <pwned-lib/passwordinspector.hpp>
+
+namespace po = boost::program_options;
 
 #ifdef WIN32
 #include <windows.h>
@@ -60,14 +63,68 @@ void setStdinEcho(bool enable)
 #endif
 }
 
+po::options_description desc("Allowed options");
+
+void hello()
+{
+  std::cout << "#pwned lookup 1.0-RC - Copyright (c) 2019 Oliver Lau" << std::endl
+            << std::endl;
+}
+
+void license()
+{
+  std::cout << "This program comes with ABSOLUTELY NO WARRANTY; for details type" << std::endl
+            << "`pwned-lookup-cli --warranty'." << std::endl
+            << "This is free software, and you are welcome to redistribute it" << std::endl
+            << "under certain conditions; see https://www.gnu.org/licenses/gpl-3.0.en.html" << std::endl
+            << "for details." << std::endl
+            << std::endl;
+}
+
+void warranty()
+{
+  std::cout << "Warranty info:" << std::endl
+            << std::endl
+            << "THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION." << std::endl
+            << std::endl
+            << "IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES." << std::endl
+            << std::endl;
+}
+
+void usage()
+{
+  std::cout << desc << std::endl;
+}
+
 int main(int argc, const char *argv[])
 {
-  if (argc < 2)
-  { // TODO: enable use of optional index file
-    std::cerr << "Usage: pwned-cli <md5_count_file>" << std::endl;
+  std::string inputFilename;
+  std::string indexFilename;
+  desc.add_options()
+  ("help", "produce help message")
+  ("input,I", po::value<std::string>(&inputFilename), "set MD5:count input file")
+  ("index,X", po::value<std::string>(&indexFilename), "set index file")
+  ("warranty", "display warranty information")
+  ("license", "display license information");
+  po::variables_map vm;
+  try
+  {
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+  }
+  catch (po::error &e)
+  {
+    std::cerr << "ERROR: " << e.what() << std::endl
+              << std::endl;
     return EXIT_FAILURE;
   }
-  pwned::PasswordInspector inspector(argv[1]);
+  po::notify(vm);
+  if (inputFilename.empty())
+  {
+    std::cerr << "ERROR: input file not given." << std::endl;
+    usage();
+    return EXIT_FAILURE;
+  }
+  pwned::PasswordInspector inspector(inputFilename, indexFilename);
   for (;;)
   {
     std::cout << "Password? ";
@@ -75,17 +132,15 @@ int main(int argc, const char *argv[])
     setStdinEcho(false);
     std::cin >> pwd;
     setStdinEcho(true);
-    if (pwd.empty())
-      break;
     const pwned::Hash soughtHash(pwd);
     std::cout << "MD5 hash " << soughtHash << std::endl;
     const auto &t0 = std::chrono::high_resolution_clock::now();
-    pwned::PasswordHashAndCount phc = inspector.smart_binsearch(soughtHash);
+    const pwned::PasswordHashAndCount &phc = inspector.binsearch(soughtHash);
     const auto &t1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t1 - t0);
     if (phc.count > 0)
     {
-      std::cout << "Found " << std::dec << phc.count << " times." << std::endl;
+      std::cout << "Found " << phc.count << " times." << std::endl;
     }
     else
     {
