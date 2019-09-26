7291ef79215cfd12d21309c1886a85769bd254c6
Oliver Lau <ola@ct.de>
2019-09-26 17:11:52 +0200

    Externalized algorithm list.

---
+++ pwned-lib/algorithms.cpp
@@ -0,0 +1,32 @@
+/*
+ Copyright © 2019 Oliver Lau <ola@ct.de>, Heise Medien GmbH & Co. KG - Redaktion c't
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <numeric>
+#include <iterator>
+
+#include "algorithms.hpp"
+
+namespace pwned
+{
+
+const std::string AlgoBinSearch = "binsearch";
+const std::string AlgoSmartBinSearch = "smart";
+const std::string AlgoMPHFSearch = "mphf";
+const std::vector<std::string> AlgoList = {AlgoBinSearch, AlgoSmartBinSearch, AlgoMPHFSearch};
+const std::string AlgoStringList = std::accumulate(std::next(AlgoList.begin()), AlgoList.end(), "'" + AlgoList.front() + "'", [](std::string a, const std::string &b) { return std::move(a) + ", '" + b + "'"; });
+
+} // namespace pwned
---
+++ pwned-lib/algorithms.hpp
@@ -0,0 +1,18 @@
+#ifndef __algorithms_hpp__
+#define __algorithms_hpp__
+
+#include <string>
+#include <vector>
+
+namespace pwned
+{
+
+extern const std::string AlgoBinSearch;
+extern const std::string AlgoSmartBinSearch;
+extern const std::string AlgoMPHFSearch;
+extern const std::vector<std::string> AlgoList;
+extern const std::string AlgoStringList;
+
+} // namespace pwned
+
+#endif // __algorithms_hpp__
