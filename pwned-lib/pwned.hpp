9bf8bb2d7a74c59c8b787abb416f9fb5c2970ab2
Oliver Lau <ola@ct.de>
2019-09-27 13:51:22 +0200

    Added pwned header file for convenience.

---
+++ pwned-lib/pwned.hpp
@@ -0,0 +1,31 @@
+/*
+ Copyright © 2019 Oliver Lau <ola@ct.de>, Heise Medien GmbH & Co. KG - Redaktion c't
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __PWNED_HPP__
+#define __PWNED_HPP__
+
+#include "hash.hpp"
+#include "operation.hpp"
+#include "operationqueue.hpp"
+#include "passwordhashandcount.hpp"
+#include "passwordinspector.hpp"
+#include "phchasher.hpp"
+#include "semaphore.hpp"
+#include "util.hpp"
+#include "uuid.hpp"
+
+#endif // __PWNED_HPP__
